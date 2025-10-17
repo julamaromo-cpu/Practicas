f6ea3c58df536740bd0cd22bad635c4549b22351
julamaromo-cpu <julamaromo@gmail.com>
2025-10-17 07:47:25 -0600

    Add files via upload

---
+++ Practica 20/index.js
@@ -0,0 +1,24 @@
+let nombre = "Julian";
+let apellido = 'Amaro';
+let nombreCompleto = nombre + ' ' + apellido;
+console.log(nombreCompleto);
+
+let numero = 250;
+let numero2 = 300;
+
+var numero3 = 100;
+var numero4 = 50; //declaraciones antiguas
+
+console.log(nombreCompleto,numero3,numero4);
+
+let suma = numero + numero2 + numero3 + numero4;
+console.log(suma);
+
+let resta = numero3 - numero2;
+console.log('El resultado es:', resta);
+
+let multiplica = numero * numero2;
+console.log('El resultado es:', multiplica);
+
+let divide = numero3 / numero4;
+console.log('El resultado de la division es:', divide)
